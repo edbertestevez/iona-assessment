7d1b0845057c1cc4705b4f5eca9e8fa48d0d67f2
Edbert Jason Estevez <ejestevez26@gmail.com>
2024-01-11 13:55:58 +0800

    refactor: utils and constants

---
+++ src/constants/messages.tsx
@@ -0,0 +1,6 @@
+// Default enum for now. Ideal to use localization
+enum MESSAGES {
+  genericError = 'Apologies but we could not load new cats for you at this time! Miau!',
+}
+
+export default MESSAGES;
